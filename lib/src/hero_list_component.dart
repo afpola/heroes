106078751911b91ebaf27d2407fb4c4e125852c5
Pontus Larsson <pontus.larsson.torner@gmail.com>
2021-07-24 01:26:32 +0200

    routing + other stuff

--- lib/src/hero_list_component.dart
+++ lib/src/hero_list_component.dart
@@ -1,16 +1,17 @@
 import 'dart:async';
 
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 
 import 'hero.dart';
-import 'hero_component.dart';
 import 'hero_service.dart';
 
 @Component(
   selector: 'my-heroes',
   templateUrl: 'hero_list_component.html',
   styleUrls: ['hero_list_component.css'],
-  directives: [coreDirectives, HeroComponent],
+  directives: [coreDirectives],
+  pipes: [commonPipes],
 )
 class HeroListComponent implements OnInit {
   final HeroService _heroService;
@@ -22,6 +23,8 @@ class HeroListComponent implements OnInit {
   Future<void> _getHeroes() async {
     heroes = await _heroService.getAll();
   }
+
+  Future<NavigationResult> gotoDetail() => null;
 
   void ngOnInit() => _getHeroes();
 
--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -4,6 +4,8 @@ import 'route_paths.dart';
 import 'hero_list_component.template.dart' as hero_list_template;
 import 'dashboard_component.template.dart' as dashboard_template;
 
+import 'hero_component.template.dart' as hero_template;
+
 export 'route_paths.dart';
 
 class Routes {
@@ -16,9 +18,16 @@ class Routes {
     routePath: RoutePaths.dashboard,
     component: dashboard_template.DashboardComponentNgFactory,
   );
+
+  static final hero = RouteDefinition(
+    routePath: RoutePaths.hero,
+    component: hero_template.HeroComponentNgFactory,
+  );
+
   static final all = <RouteDefinition>[
     heroes,
     dashboard,
+    hero,
     RouteDefinition.redirect(
       path: '',
       redirectTo: RoutePaths.dashboard.toUrl(),
